668066df58e15a898883986ae07c5faa4c5314d6
Elias Rubin <erubin@princeton.edu>
2015-06-16 11:04:09 -0400

    got chisq.py to work

--- day5/exercises/chisq.py
+++ day5/exercises/chisq.py
@@ -3,11 +3,10 @@ import numpy as np
 import pandas as pd
 
 
-def guess(x, y, n_points):
+def guess(x, y):
 
-    '''accepts x and y (array-like) and returns a grid 
-    for the linear parameter space based on a crude guess'''
-
+    '''accepts x and y (array-like) and returns an initial guess 
+    for the slope and intercept'''
 
     slope0 = (max(y) - min(y)) / (max(x) - min(x))
     intercept0 = ((max(y)) + (min(y))) / 2.
@@ -18,8 +17,8 @@ def guess(x, y, n_points):
 
 def grid_generator(slope, intercept, n_points, n_iteration):
 
-    min_slope = (1 - (0.5**n_iteration) * slope)
-    max_slope = (1 + (0.5**n_iteration) * slope)
+    min_slope = (1 - (0.5**n_iteration)) * slope
+    max_slope = (1 + (0.5**n_iteration)) * slope
 
     min_intercept = (1 - (0.5**n_iteration)) * intercept 
     max_intercept = (1 + (0.5**n_iteration)) * intercept
@@ -34,16 +33,16 @@ def lin_model(x, slope, intercept):
     '''accepts array-like x and numbers slope and intercept and returns array-like y
     using a linear model'''
 
-    return slope * x + intercept
+    return (slope * x + intercept)
 
 def one_d_chisq(y, y_predict, y_error):
 
     '''accepts three array-like arguments of data, model, and error data
     and computes chi squared'''
 
-    return sum(((y - y_predict)/y_error)**2)
+    return np.sum((y - y_predict)**2 / (y_error)**2)
 
-def chi_sq_generator(x, y, y_error, slopes, intercepts):
+def chi_sq_generator(x, y, y_error, slopes, intercepts, chisq_array):
 
     chisq = float('inf')
     slope = 0
@@ -52,40 +51,47 @@ def chi_sq_generator(x, y, y_error, slopes, intercepts):
     for i in slopes:
         for j in intercepts:
             test_chisq = one_d_chisq(y,lin_model(x, i, j), y_error)
+            #print chisq
+            #print slope
             if test_chisq < chisq:
                 chisq = test_chisq
                 slope = i
                 intercept = j
 
-    return chisq, i, j
+    return chisq, slope, intercept
+
+def adaptive_generator(x,y,y_error, slope, intercept, chisq_array, tolerance, n_points, n_iteration):
+    result = []
+
+    def adaptive_generator_rec(x, y, y_error, slope, intercept, chisq_array, tolerance, n_points, n_iteration):
+        
+        '''uses adaptive mesh refinement to obtain the 
+        parameters to a user specificed tolerance in chisq'''
+
+        grid = grid_generator(slope, intercept, n_points, n_iteration)
+
+        state = chi_sq_generator(x,y,y_error,grid[0],grid[1],chisq_array)
+
+        n_iteration += 1
+
+        chisq_array[1] = chisq_array[0]
+
+        chisq_array[0] = state[0]
+
+        slope = state[1]
+
+        intercept  = state[2]
 
 
-def adaptive_generator(x, y, y_error, slope, intercept, chisq_array, tolerance, n_points, n_iteration):
+        if abs(chisq_array[0] - chisq_array[1]) < tolerance:
+            result.append(state)
+            return
 
-    '''uses adaptive mesh refinement to obtain the 
-    parameters to a user specificed tolerance in chisq'''
-
-    grid = grid_generator(slope, intercept, n_points, n_iteration)
-
-    state = chi_sq_generator(x,y,y_error,grid[0],grid[1])
-
-    n_iteration += 1
-
-    chisq_array[1] = chisq_array[0]
-
-    chisq_array[0] = state[0]
-
-    slope = state[1]
-
-    intercept  = state[2]
-
-
-
-    if abs(chisq_array[0] - chisq_array[1]) < tolerance:
-        return state
-
-    else:
-        adaptive_generator(x,y,y_error,slope,intercept,chisq_array,tolerance,n_points,n_iteration)
+        else:
+            adaptive_generator_rec(x,y,y_error,slope,intercept,chisq_array,tolerance,n_points,n_iteration)
+    
+    adaptive_generator_rec(x,y,y_error, slope, intercept, chisq_array, tolerance, n_points, n_iteration)
+    return result
 
 
 if __name__ == '__main__':
@@ -96,9 +102,11 @@ if __name__ == '__main__':
     weights = df['Weight']
     weight_err = df['Weight errors']
 
-    state0 = guess(heights, weights, 1000)
-    chisq_array = np.zeros(2)
-    adaptive_generator(heights, weights, weight_err, state0[0], state0[1], chisq_array, 1e-4, 1000, 1)
+    state0 = [8, -250]#guess(heights, weights)
 
 
+    chisq_array = [0.0,0.0]
+    a = adaptive_generator(heights, weights, weight_err, state0[0], state0[1], chisq_array, 1e-6, 10, 1)
+    
+    print a[0]
 
